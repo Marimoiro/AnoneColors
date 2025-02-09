282cfc8cab65cf83719e34545ea1dda1befa9994
marimoiro <5342721+Marimoiro@users.noreply.github.com>
2025-02-09 23:20:04 +0900

    first commit

---
+++ src/pages/PatternCircle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+
+  <!-- 円を描く -->
+  <div class="circle"></div>
+
+</template>
+
+<style scoped>
+
+/* cssで円を描く */
+.circle {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #f00;
+}
+
+
+
+</style>
